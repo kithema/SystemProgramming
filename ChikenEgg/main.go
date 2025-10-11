97b856f1349e1bda0dd28a9f5ef61e333bce39c7
kithema <k1th3ma@gmail.com>
2025-10-11 11:07:55 +0300

    added comments

--- ChikenEgg/main.go
+++ ChikenEgg/main.go
@@ -4,20 +4,21 @@ import (
 	"fmt"
 	"time"
 )
-
+//Создание Курицы и Яйца
 var chiken = object{"Курица"}
 var egg = object{"Яйцо"}
+//Создание буферизованого канала
 var Done = make(chan string, 1)
 
 func main() {
 
 	go chiken.run()
 	go egg.run()
-
+	//Пропуск первого завершившего свою работу
 	<-Done
+	//Вывод второго завершившего свою работу
 	fmt.Printf("Спор решен! %s появилось первым", <-Done)
 }
-
 type object struct {
 	name string
 }
@@ -28,5 +29,7 @@ func (o object) run() {
 		time.Sleep(time.Millisecond)
 		fmt.Printf("%s\n", o.name)
 	}
+	//Отправка имени в канал
 	Done <- fmt.Sprintf(o.name)
 }
+
