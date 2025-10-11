a41c4d23ea096fdba446ec1f4299462da9f07c51
kithema <k1th3ma@gmail.com>
2025-10-11 11:03:39 +0300

    add comments

--- 10go/main.go
+++ 10go/main.go
@@ -8,19 +8,23 @@ import (
 )
 
 func main() {
+	//Создание переменной стркутурые WaitGroup
     var wg sync.WaitGroup
-    
+    //Цикл с зупском 10 горутин
     for i := 0; i < 10; i++ {
+		//Добавление горутины в ожидание
         wg.Add(1)
         go func(id int) {
 			seconds := rand.Intn(30)
             fmt.Printf("Горутина %d запущена\n", id)
             time.Sleep(time.Second * time.Duration(seconds))//очень сильно работаем
             fmt.Printf("Горутина %d завершена, с множителем %d\n", id, seconds)
+			//Сигнал завершения 
             wg.Done()
         }(i)
     }
-    
+    //Ожидаем завершение всех горутин
     wg.Wait()
     fmt.Println("Все горутины завершены")
 }
+
