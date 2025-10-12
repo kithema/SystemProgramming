84ce9fe84fd581500b534cb3c7bacc79a9b9c99e
kithema <k1th3ma@gmail.com>
2025-10-12 12:48:27 +0300

    added comments

--- Choco/main.go
+++ Choco/main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	var money, price, wrap int
-
+	//Ввод данных с клавитауры
 	fmt.Print("Введите количество денег: ")
 	fmt.Scan(&money)
 	fmt.Print("Введите цену за 1 шоколадку: ")
 	fmt.Scan(&price)
 	fmt.Print("Введите количество оберток для бесплатной шоколадки: ")
 	fmt.Scan(&wrap)
-
+	//проверка вводных
 	if price <= 0 || wrap <= 0 {
 		fmt.Println("Ошибка: цена и количество оберток должны быть больше 0")
 		return
@@ -20,22 +20,22 @@ func main() {
 	total := recursiveChocolate(money, price, wrap, 0)
 	fmt.Printf("Всего можно получить шоколадок: %d\n", total)
 }
-
+//рекурсивная функция для подсчета
 func recursiveChocolate(money, price, wrap, wrappers int) int {
-
+	//если нельзя купить и поменять
 	if money < price && wrappers < wrap {
 		return 0
 	}
 
 	var chocolates int
-
+	//закупка шоколадок за деньги
 	if money >= price {
 		chocolates = money / price
 		money = money % price
 		wrappers += chocolates
 		fmt.Printf("Куплено %d шоколадок. Оберток: %d\n", chocolates, wrappers)
 	}
-
+	//закупка за обертки
 	if wrappers >= wrap {
 		newChocolates := wrappers / wrap
 		remainingWrappers := wrappers % wrap
@@ -46,3 +46,4 @@ func recursiveChocolate(money, price, wrap, wrappers int) int {
 
 	return chocolates
 }
+
